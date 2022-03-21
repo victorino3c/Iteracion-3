b73fdc23c6a64bbd1dcc1ce57871cca936e18ec7
omg-56 <99333483+omg-56@users.noreply.github.com>
2022-03-21 18:53:27 +0100

    Update command.c

--- command.c
+++ command.c
@@ -19,19 +19,19 @@
 #define CMD_LENGHT 30
 
 char *cmd_to_str[N_CMD]
-[N_CMDT] = {{"", "No command"},    /*!< Al empezar, no hay comandos */
-      {"", "Unknown"},             /*!< Si no recibe nada o algun caracter desconocido, se interpreta como desconocido */
-      {"e", "Exit"},               /*!< Si recibe una e, se interpreta como Exit*/
-      {"s", "Down"},               /*!< Si recibe una s, se interpreta como Down/Next */
-      {"a", "Left"},               /*!< Si recibe una a, se interpreta como Left */
-      {"w", "Up"},               /*!< Si recibe una w, se interpreta como Up/Back */
-      {"d", "Right"},              /*!< Si recibe una d, se interpreta como Right */
-      {"c", "Take"},               /*!< Si recibe una z, se interpreta como Take */
-      {"v", "Drop"},               /*!< Si recibe una x, se interpreta como Drop */
-      {"q", "Attack"}};            /*!< Si recibe una q, se interpreta como Attack */
+[N_CMDT] = {{"", "No command"},    /*!< At the beginning, there is no commands */
+      {"", "Unknown"},             /*!< If an empty or unknown string is received, it is interpreted as Unknown */
+      {"e", "Exit"},               /*!< If an e is received, it is interpreted as Exit */
+      {"s", "Down"},               /*!< If a s is received, it is interpreted as Down/Next */
+      {"a", "Left"},               /*!< If an a is received, it is interpreted as Left */
+      {"w", "Up"},                 /*!<  If a w is received, it is interpreted as Up/Back */
+      {"d", "Right"},              /*!< If a d is received, it is interpreted as Right */
+      {"c", "Take"},               /*!<  If a c is received, it is interpreted as Take */
+      {"v", "Drop"},               /*!<  If a v is received, it is interpreted as Drop */
+      {"q", "Attack"}};            /*!<  If a s is received, it is interpreted as Attack */
 
-/**Escanea el cmd en busca de caracteres clave que introduzca el usuario 
-  * para interpretar y clasificar la informacion
+/**It scans the cmd searching for key words introduced by the user 
+  *in order to interpret and clasify the info
   */
 T_Command command_get_user_input(char *arg)
 {
@@ -48,11 +48,11 @@ T_Command command_get_user_input(char *arg)
   if (scanf("%s", input) > 0)
   {
     cmd = UNKNOWN;
-    while (cmd == UNKNOWN && i < N_CMD)     /*!< Condicion para seguir leyendo el cmd*/
+    while (cmd == UNKNOWN && i < N_CMD)     /*!< Condition to continue reading */
     {
       if (!strcasecmp(input, cmd_to_str[i][CMDS]) || !strcasecmp(input, cmd_to_str[i][CMDL]))
       {
-        cmd = i + NO_CMD;                   /*!< Si detecta diferencias entre CMDS y CMDL con el input, modificar cmd */
+        cmd = i + NO_CMD;                   /*!< If any differences are detected between CMDS, CMDL and the input, cmd is modified */
         if (cmd == TAKE || cmd == DROP )
         {
           if (scanf("%s", arg) < 0)
@@ -62,7 +62,7 @@ T_Command command_get_user_input(char *arg)
         }
         
       }
-      else                                  /*!< En caso contrario seguir avanzando en la lectura */
+      else                                  /*!< In any other case, continue reading >! */
       {
         i++;                                 
       }
