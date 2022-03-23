02fe6bdab9858d6175045a91a2dfbb7303e4bd56
victorino3c <99192011+victorino3c@users.noreply.github.com>
2022-03-23 12:59:55 +0100

    Update game_loop.c

--- game_loop.c
+++ game_loop.c
@@ -17,23 +17,21 @@
 
 
 int game_loop_init(Game *game, Graphic_engine **gengine, char *file_name);
-void game_loop_run(Game game, Graphic_engine *gengine);
-void game_loop_cleanup(Game game, Graphic_engine *gengine);
+void game_loop_run(Game *game, Graphic_engine *gengine);
+void game_loop_cleanup(Game *game, Graphic_engine *gengine);
 
 /**
-  * @brief Main game_loop program
+  * @brief Programa principal del game_loop
   * @author Profesores PPROG
   *
-  * Checks whether argc and argv values are the expected in order 
-  * to initialize the game and/or the cleanup
-  * @param argc an integer
-  * @param argv a string
-  * @return 0 if the game is succesfully initialized and 1 
-  * if game_data_file was not the expected one
+  * Comprueba si el valor de argc y argv son correctos para decidir si inciar el juego y activar el cleanup
+  * @param argc un entero
+  * @param argv una cadena de caracteres
+  * @return 0 si se ejecuta el juego con exito y 1 si no se ha usado el game_data_file correcto
   */
 int main(int argc, char *argv[])
 {
-  Game game;
+  Game *game = game_alloc2();
   Graphic_engine *gengine;
 
   if (argc < 2)
@@ -42,7 +40,7 @@ int main(int argc, char *argv[])
     return 1;
   }
  
-  if (!game_loop_init(&game, &gengine, argv[1]))
+  if (!game_loop_init(game, &gengine, argv[1]))
   {
     game_loop_run(game, gengine);
     game_loop_cleanup(game, gengine);
@@ -52,16 +50,15 @@ int main(int argc, char *argv[])
 }
 
 /**
-  * @brief Initializes the game and all its members 
+  * @brief Inicializa el juego
   * @author Profesores PPROG
   *
-  * @param game pointer to game that contains every needed member 
-  * for the main execution of the programme.
-  * @param gengine double pointer to access the graphic engine
-  * @param file_name pointer to the file's name from which the game 
-  * is created
-  * @return 0 if the game is succesfully initialized and 1 if anything 
-  * goes wrong
+  * Comprueba si el valor de argc y argv son correctos para decidir si inciar el juego y activar el cleanup
+  * @param game es el puntero que apunta a la estructura tipo Game que contiene los datos de localización de objeto y jugador 
+  * junto con los espacios del juego y el ultimo comando
+  * @param gengine es un doble puntero para acceder al motor grafico
+  * @param file_name es un puntero al nombre del fichero para acceder a el tras haber ejecutado el juego e imprimir cualquier error que se haya dado
+  * @return 0 si se inicializa el juego con exito y 1 si no se ha producido un error
   */
 int game_loop_init(Game *game, Graphic_engine **gengine, char *file_name)
 {  
@@ -82,44 +79,40 @@ int game_loop_init(Game *game, Graphic_engine **gengine, char *file_name)
 }
 
 /**
-  * @brief Keeps the game running
+  * @brief Ejecuta el juego
   * @author Profesores PPROG
   *
-  * Loop that is constantly checking whether the exit 
-  * command is introduced or game_is_over is TRUE so 
-  * that the game keeps running or not 
-  * @param game pointer to game that contains every needed member 
-  * for the main execution of the programme.
-  * @param gengine pointer to the graphic engine
+  * Comprueba en bucle si no se ha introducido exit ni el juego se ha acabado para seguir ejecutando el juego
+  * @param game es el puntero que apunta a la estructura tipo Game que contiene los datos de localización de objeto y jugador 
+  * junto con los espacios del juego y el ultimo comando
+  * @param gengine es un puntero que apunta al motor grafico
   */
-void game_loop_run(Game game, Graphic_engine *gengine)
+void game_loop_run(Game *game, Graphic_engine *gengine)
 {
   T_Command command = NO_CMD;
   char arg[MAX_ARG];
   int st = 5;
 
-  while ((command != EXIT) && !game_is_over(&game))
+  while ((command != EXIT) && !game_is_over(game))
   {
-    graphic_engine_paint_game(gengine, &game, st);
+    graphic_engine_paint_game(gengine, game, st);
     command = command_get_user_input(arg);
-    st = game_update(&game, command, arg);
+    st = game_update(game, command, arg);
   }
 }
 
 
 /**
-  * @brief Ends and destroys all of the game's components 
+  * @brief Termina y limpia el juego
   * @author Profesores PPROG
   *
-  * After ending the current game, it destroys its components
-  * by calling the suitable functions which also will free all
-  * allocated memory.
-  * @param game pointer to game that contains every needed member 
-  * for the main execution of the programme.
-  * @param gengine pointer to the graphic engine
+  * Tras terminar con la partida actual, destruye el juego y el motor grafico para limpiar estos procesos
+  * @param game es el puntero que apunta a la estructura tipo Game que contiene los datos de localización de objeto y jugador 
+  * junto con los espacios del juego y el ultimo comando
+  * @param gengine es un puntero que apunta al motor grafico
   */
-void game_loop_cleanup(Game game, Graphic_engine *gengine)
+void game_loop_cleanup(Game *game, Graphic_engine *gengine)
 {
-  game_destroy(&game);
+  game_destroy(game);
   graphic_engine_destroy(gengine);
 }
