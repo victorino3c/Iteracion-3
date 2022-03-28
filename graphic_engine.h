57a191c24e7fbe0493ee51d5ff24fdb17f3564e9
omg-56 <99333483+omg-56@users.noreply.github.com>
2022-03-28 13:38:15 +0200

    Add files via upload

--- graphic_engine.h
+++ graphic_engine.h
@@ -14,53 +14,54 @@
 #include "game.h"
 
 /**
- * @brief Motor grafico
+ * @brief Graphic engine
  *
- * Esta estructura contiene todos los punteros a los recursos necesarios que gestiona
- * el motor grafico como el mapa, help o feedback
+ * This structure contains all pointers and data from the needed resources
+ * to work with the graphic engine and all its members, them being everything
+ * the user sees printed in their screen
+ *
  */
 typedef struct _Graphic_engine Graphic_engine;
 
 /**
-  * @brief Crea el motor grafico 
+  * @brief Creates the graphic engine 
   * @author Profesores PPROG
   *
-  * graphic_engine_create inicializa la pantalla de juego con las dimensiones adecuadas
-  * y reserva memoria para una variable ge del tipo Graphic_engine (motor grafico) 
-  * e inicializa en las posiciones iniciales el mapa, el menu de ayuda y los otros elementos de la variable.
-  * @return ge si todo sale bien y NULL si hubo un error en la reserva de memoria
+  * graphic_engine_create initializes the gamescreen with specific screen dimensions,
+  * allocs memory for a variable, ge, type Graphic_engine and it also initializes
+  * each member in its position on said screen 
+  * @return ge if everything goes well or NULL if something went wrong
   */
 Graphic_engine *graphic_engine_create();
 
 /**
-  * @brief Destruye los elementos de ge
+  * @brief Destroys every member of the graphic engine
   * @author Profesores PPROG
   *
-  * graphic_engine_destroy, llamando a las funciones de screen_area_destroy, 
-  * destruye mapa, banner, descript, help y feedback 
-  * Ademas libera la memoria que se habia reservado para ge
-  * @param ge el puntero que apunta al motor grafico, otorga acceso a modificar sus componentes
+  * graphic_engine_destroy, using the function screen_area_destroy, 
+  * destroys the map, banner, descript, help and feedback 
+  * It also frees all the previosly alloc'd memory
+  * @param ge pointer to the graphic engine
   */
 void graphic_engine_destroy(Graphic_engine *ge);
 
 /**
-  * @brief Dibuja el juego 
+  * @brief Prints the game on the screen
   * @author Profesores PPROG
   *
-  * El motor grafico dibuja en la inerfaz los componentes del juego para que se visualicen
-  * en la posicion correcta en la pantalla
-  * @param ge el puntero que apunta al motor grafico, otorga acceso a modificar sus componentes
-  * @param game es el puntero que apunta a game, que contiene los datos de localización de objeto y jugador 
-  * junto con los espacios del juego y el ultimo comando
+  * Prints the game on the screen, with all its components on the 
+  * right position for the user to see the info needed to interact with it
+  * @param ge pointer to the graphic engine
+  * @param game pointer to the already created game
   */
 void graphic_engine_paint_game(Graphic_engine *ge, Game *game, int st);
 
 /**
-  * @brief Escribe el comando introducido en la interfaz grafica
+  * @brief Writes down the last inputted command
   * @author Profesores PPROG
   *
-  * @param ge el puntero que apunta al motor grafico, otorga acceso a modificar sus componentes
-  * @param str el puntero que apunta a la cadena de caracteres introducida por el usuario.
+  * @param ge pointer to the graphic engine
+  * @param str pointer to the introduced string
   */
 void graphic_engine_write_command(Graphic_engine *ge, char *str);
 
