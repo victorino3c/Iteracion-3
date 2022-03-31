335106cd7f9dab0a02df4601304d0ce76a5a2903
msoto5 <99289966+msoto5@users.noreply.github.com>
2022-03-31 17:45:47 +0200

    Update space.c

--- space.c
+++ space.c
@@ -1,10 +1,10 @@
-/** 
+/**
  * @brief It implements the space module
- * 
+ *
  * @file space.c
  * @author Profesores PPROG
- * @version 2.0 
- * @date 29-11-2021 
+ * @version 2.0
+ * @date 29-11-2021
  * @copyright GNU Public License
  */
 
@@ -15,87 +15,150 @@
 
 #include "space.h"
 
+#define MAX_LINKS_SPACE 4
 
 /**
  * @brief Space
  *
  * This struct stores all the information of a space.
  */
-struct _Space {
-  Id id;                                    /*!< Id number of the space, it must be unique */
-  char name[WORD_SIZE + 1];                 /*!< Name of the space */
-  char description[WORD_SIZE];              /*!< Description of the space*/
-  Id north;                                 /*!< Id of the space at the north */
-  Id south;                                 /*!< Id of the space at the south */
-  Id east;                                  /*!< Id of the space at the east */
-  Id west;                                  /*!< Id of the space at the west */
-  Set *objects;                             /*!< Conjunto de ids de los objetos que se encuentran en el espacio */
-  char **gdesc;                             /*!< Array de 5 strings de 9 caracteres */
+struct _Space
+{
+  Id id;                         /*!< Id number of the space, it must be unique */
+  char name[WORD_SIZE + 1];      /*!< Name of the space */
+  char description[WORD_SIZE];   /*!< Description of the space*/
+  Id link[MAX_LINKS_SPACE];      /*!< Id from links between space with this space as origin */
+  Set *objects;                  /*!< Conjunto de ids de los objetos que se encuentran en el espacio */
+  char **gdesc;                  /*!< Array de 5 strings de 9 caracteres */
 };
 
+/**
+ * Private functions
+ */
+int _dir2i(DIRECTION dir);
+
+/**
+ * @brief Given a direction returns position of links array
+ * @author Miguel Soto
+ * 
+ * LINK ARRAY INDEXES:
+ * (NORTH) link[0]; 
+ * (EAST) link[1]; 
+ * (SOUTH) link[2]; 
+ * (WEST) link[3];
+ * 
+ * @param dir direccion 
+ * @return index input in link array
+ */
+int _dir2i(DIRECTION dir)
+{
+  int n;
+  
+  if (dir == N)
+  {
+    n = 0;
+  }
+  else if (dir == E)
+  {
+    n = 1;
+  }
+  else if (dir == S)
+  {
+    n = 2;
+  }
+  else if (dir == W)
+  {
+    n = 3;
+  }
+  else
+  {
+    n = -1;
+  }
+  
+  return n;
+}
+
 /** space_create allocates memory for a new space
-  *  and initializes its members
-  */
-Space* space_create(Id id) {
+ *  and initializes its members
+ */
+Space *space_create(Id id)
+{
   Space *newSpace = NULL;
+  int i;
 
   /* Error control */
   if (id == NO_ID)
     return NULL;
 
-  newSpace = (Space *) malloc(sizeof (Space));
-  if (newSpace == NULL) {
+  newSpace = (Space *)malloc(sizeof(Space));
+  if (newSpace == NULL)
+  {
     return NULL;
   }
 
   /* Initialization of an empty space*/
   newSpace->id = id;
   newSpace->name[0] = '\0';
-  newSpace->north = NO_ID;
-  newSpace->south = NO_ID;
-  newSpace->east = NO_ID;
-  newSpace->west = NO_ID;
+  newSpace->description[0] = '\0';
+  for (i = 0; i < 4; i++)
+  {
+    newSpace->link[i] = NULL;
+  }
   newSpace->objects = set_create();
   newSpace->gdesc = NULL;
 
   return newSpace;
 }
 
-/** space_destroy frees the previous memory allocation 
-  *  for a space
-  */
-STATUS space_destroy(Space* space) 
+/** space_destroy frees the previous memory allocation
+ *  for a space
+ */
+STATUS space_destroy(Space *space)
 {
+  int i;
+  STATUS st = OK;
+
   // Error control
-  if (!space) {
+  if (!space)
+  {
     return ERROR;
   }
 
+  for (i = 0; i < 4; i++)
+  {
+    link_destroy(space->link[i]);
+    space->link[i] = NULL;
+  }
+  
   if (space->objects)
   {
-    set_destroy(space->objects);
+    if (!set_destroy(space->objects))
+    {
+      return NULL;
+    }
     space->objects = NULL;
   }
 
-  
   if (space->gdesc)
   {
     if (space_destroy_gdesc(space->gdesc) == ERROR)
     {
       /*printf("ERROR liberando memoria gdesc\n");*/
+      return NULL;
     }
   }
-  
- 
+
   free(space);
   space = NULL;
   return OK;
 }
 
 /** It gets the id of a space
-  */
-Id space_get_id(Space* space) {
-  if (!space) {
+ */
+Id space_get_id(Space *space)
+{
+  if (!space)
+  {
     return NO_ID;
   }
   return space->id;
@@ -115,11 +178,11 @@ STATUS space_test_id(Id id)
   }
 
   // Calcular numbero total de digitos - 1
-  digits = (int)log10(id); 
+  digits = (int)log10(id);
 
   // Obtener primer digito
   first_digit = (int)(id / pow(10, digits));
-  
+
   if (first_digit == FD_ID_SPACE)
   {
     return OK;
@@ -131,21 +194,27 @@ STATUS space_test_id(Id id)
 }
 
 /** It sets the name of a space
-  */
-STATUS space_set_name(Space* space, char* name) {
-  if (!space || !name) {
+ */
+STATUS space_set_name(Space *space, char *name)
+{
+  if (!space || !name)
+  {
     return ERROR;
   }
 
-  if (!strcpy(space->name, name)) {
+  if (!strcpy(space->name, name))
+  {
     return ERROR;
   }
   return OK;
 }
+
 /** It gets the name of a space
-  */
-const char * space_get_name(Space* space) {
-  if (!space) {
+ */
+const char *space_get_name(Space *space)
+{
+  if (!space)
+  {
     return NULL;
   }
   return space->name;
@@ -155,12 +224,12 @@ const char * space_get_name(Space* space) {
  */
 const char *space_get_description(Space *space)
 {
-	/*CONTROL ERRORS*/
+  /*CONTROL ERRORS*/
   if (!space)
   {
     return NULL;
   }
-  
+
   return space->description;
 }
 
@@ -168,92 +237,58 @@ const char *space_get_description(Space *space)
  */
 STATUS space_set_description(Space *space, char *description)
 {
-	/* CONTROL ERROR */
+  /* CONTROL ERROR */
   if (!space || !description)
   {
     return ERROR;
   }
-  
+
   if (!strcpy(space->description, description))
   {
     /*CONTROL ERROR*/
     return ERROR;
   }
+
+  return OK;
+}
+
+/**
+ * It sets the link of the space in an specific direction
+ */
+STATUS space_set_link(Space *space, Id link, DIRECTION dir)
+{
+  if (!space || !link || dir == ND)
+  {
+    return ERROR;
+  }
   
-  return OK;
-
-}
-
-/** It sets the id of the space located at the north
-  */
-STATUS space_set_north(Space* space, Id id) {
-  if (!space || id == NO_ID) {
+  int n = _dir2i(dir);
+  if (n < 0)
+  {
     return ERROR;
   }
-  space->north = id;
+  
+  space->link[n] = link;
   return OK;
-}
-/** It gets the id of the space located at the north
-  */
-Id space_get_north(Space* space) {
-  if (!space) {
-    return NO_ID;
-  }
-  return space->north;
 }
 
-/** It sets the id of the space located at the south
-  */
-STATUS space_set_south(Space* space, Id id) {
-  if (!space || id == NO_ID) {
-    return ERROR;
+/**
+ * It gets link's id that starts in this space with an specific direction
+ */
+Id space_get_link(Space *space, DIRECTION dir)
+{
+  if (!space || dir == ND)
+  {
+    return -1;
   }
-  space->south = id;
-  return OK;
-}
-/** It gets the id of the space located at the south
-  */
-Id space_get_south(Space* space) {
-  if (!space) {
-    return NO_ID;
+  
+  int n = _dir2i(dir);
+  if (n < 0)
+  {
+    return -1;
   }
-  return space->south;
-}
-
-/** It sets the id of the space located at the east
-  */
-STATUS space_set_east(Space* space, Id id) {
-  if (!space || id == NO_ID) {
-    return ERROR;
-  }
-  space->east = id;
-  return OK;
-}
-/** It gets the id of the space located at the east
-  */
-Id space_get_east(Space* space) {
-  if (!space) {
-    return NO_ID;
-  }
-  return space->east;
-}
-
-/** It sets the id of the space located at the west
-  */
-STATUS space_set_west(Space* space, Id id) {
-  if (!space || id == NO_ID) {
-    return ERROR;
-  }
-  space->west = id;
-  return OK;
-}
-/** It gets the id of the space located at the west
-  */
-Id space_get_west(Space* space) {
-  if (!space) {
-    return NO_ID;
-  }
-  return space->west;
+  
+  return space->link[n];
 }
 
 /**
@@ -266,7 +301,7 @@ STATUS space_add_objectid(Space *s, Id id)
   {
     return ERROR;
   }
-  
+
   if (!set_add(s->objects, id))
   {
     return ERROR;
@@ -285,22 +320,23 @@ STATUS space_del_objectid(Space *s, Id id)
   {
     return ERROR;
   }
-  
+
   if (!set_del_id(s->objects, id))
   {
     return ERROR;
   }
-  
+
   return OK;
 }
 
-/** 
+/**
  * It gets a pointer to the set of objects in a space
  */
 Set *space_get_objects(Space *space)
 {
   // Error control
-  if (!space) {
+  if (!space)
+  {
     return NULL;
   }
   return space->objects;
@@ -316,7 +352,7 @@ Id *space_get_objects_ids(Space *s)
   {
     return NULL;
   }
-  
+
   return set_get_ids(space_get_objects(s));
 }
 
@@ -334,21 +370,21 @@ BOOL space_has_object(Space *s, Id id)
   {
     return FALSE;
   }
-  
+
   ids = set_get_ids(s->objects);
   if (ids == NULL)
   {
     return FALSE;
   }
-  
+
   for (i = 0; (i < set_get_nids(s->objects)) && (b == FALSE); i++)
   {
-    if (*(ids+i) == id)
+    if (*(ids + i) == id)
     {
       b = TRUE;
     }
   }
-  
+
   return b;
 }
 
@@ -357,30 +393,28 @@ char **space_create_gdesc()
   char **newgdesc = NULL;
   int i, j;
 
-  newgdesc = (char **) malloc((TAM_GDESC_Y+1) * sizeof(char *));
+  newgdesc = (char **)malloc((TAM_GDESC_Y + 1) * sizeof(char *));
   if (!newgdesc)
   {
     return NULL;
   }
-  for (i = 0; i < (TAM_GDESC_Y+1); i++)
+  for (i = 0; i < (TAM_GDESC_Y + 1); i++)
   {
     newgdesc[i] = NULL;
-    newgdesc[i] = (char *) malloc((TAM_GDESC_X+2) * sizeof(char));
+    newgdesc[i] = (char *)malloc((TAM_GDESC_X + 2) * sizeof(char));
     if (!newgdesc[i])
     {
       return NULL;
     }
-    
-    for (j = 0; j < (TAM_GDESC_X+2); j++)
+
+    for (j = 0; j < (TAM_GDESC_X + 2); j++)
     {
       newgdesc[i][j] = '\0';
     }
-    
   }
 
   return newgdesc;
 }
-
 
 STATUS space_destroy_gdesc(char **gdesc)
 {
@@ -391,7 +425,7 @@ STATUS space_destroy_gdesc(char **gdesc)
   {
     return ERROR;
   }
-  
+
   if (gdesc)
   {
     for (i = TAM_GDESC_Y; i >= 0; i--)
@@ -409,7 +443,6 @@ STATUS space_destroy_gdesc(char **gdesc)
 
   return OK;
 }
-
 
 /**
  * It gets the graphic description from a space.
@@ -439,50 +472,67 @@ char **space_get_gdesc(Space *s)
 }
 
 /** It prints the space information
-  */
-STATUS space_print(Space* space) {
+ */
+STATUS space_print(Space *space)
+{
   Id idaux = NO_ID;
   int nobj, i, j;
 
   /* Error Control */
-  if (!space) {
+  if (!space)
+  {
     return ERROR;
   }
 
   /* 1. Print the id and the name of the space */
   fprintf(stdout, "--> Space (Id: %ld; Name: %s)\n", space->id, space->name);
- 
-  /* 2. For each direction, print its link */ 
+
+  /* 2. For each direction, print its link */
   idaux = space_get_north(space);
-  if (NO_ID != idaux) {
+  if (NO_ID != idaux)
+  {
     fprintf(stdout, "---> North link: %ld.\n", idaux);
-  } else {
+  }
+  else
+  {
     fprintf(stdout, "---> No north link.\n");
   }
   idaux = space_get_south(space);
-  if (NO_ID != idaux) {
+  if (NO_ID != idaux)
+  {
     fprintf(stdout, "---> South link: %ld.\n", idaux);
-  } else {
+  }
+  else
+  {
     fprintf(stdout, "---> No south link.\n");
   }
   idaux = space_get_east(space);
-  if (NO_ID != idaux) {
+  if (NO_ID != idaux)
+  {
     fprintf(stdout, "---> East link: %ld.\n", idaux);
-  } else {
+  }
+  else
+  {
     fprintf(stdout, "---> No east link.\n");
   }
   idaux = space_get_west(space);
-  if (NO_ID != idaux) {
+  if (NO_ID != idaux)
+  {
     fprintf(stdout, "---> West link: %ld.\n", idaux);
-  } else {
+  }
+  else
+  {
     fprintf(stdout, "---> No west link.\n");
   }
 
   /* 3. Print if there is an object in the space or not */
   nobj = set_get_nids(space_get_objects(space));
-  if (nobj > 0) {
+  if (nobj > 0)
+  {
     fprintf(stdout, "---> %d objects in the space.\n", nobj);
-  } else {
+  }
+  else
+  {
     fprintf(stdout, "---> No object in the space.\n");
   }
 
@@ -500,6 +550,6 @@ STATUS space_print(Space* space) {
       printf("ERROR IMPRIMIENDO space->gdesc[%d][%d] = NULL\n", i, j);
     }
   }
-  
+
   return OK;
 }
