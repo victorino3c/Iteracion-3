2694c57b0be63a507c9577c5d3d629084de93120
msoto5 <99289966+msoto5@users.noreply.github.com>
2022-04-02 18:37:08 +0200

    Update types.h

--- types.h
+++ types.h
@@ -2,7 +2,7 @@
  * @brief It defines common types for the whole project
  *
  * @file types.h
- * @author Profesores PPROG
+ * @author Miguel Soto, Nicolas Victorino, Antonio Van-Oers, Ignacio Nunez
  * @version 2.0
  * @date 29-11-2021
  * @copyright GNU Public License
@@ -16,12 +16,10 @@
 #define MAX_ARG 20
 
 // Establecer norma primer digito (fd = first digit) de los ids
-#define FD_ID_SPACE 1       /* <! First digit of a space  */
-#define FD_ID_PLAYER 2      /* <! First digit of a player  */
-#define FD_ID_OBJ 3         /* <! First digit of an object*/
-#define FD_ID_ENEMY 4       /* <! First digit of an enemy*/
-
-
+#define FD_ID_SPACE 1  /* <! First digit of a space  */
+#define FD_ID_PLAYER 2 /* <! First digit of a player  */
+#define FD_ID_OBJ 3    /* <! First digit of an object*/
+#define FD_ID_ENEMY 4  /* <! First digit of an enemy*/
 
 typedef long Id;
 
@@ -39,11 +37,11 @@ typedef enum
 
 typedef enum
 {
-  N,    /* !< Short for North */
-  S,    /* !< Short for South */
-  E,    /* !< Short for East */
-  W,     /* !< Short for West */
-  ND    /* !< Short for Not Defined */
+  ND = -1, /* !< Short for Not Defined */
+  N,       /* !< Short for North */
+  S,       /* !< Short for South */
+  E,       /* !< Short for East */
+  W        /* !< Short for West */
 } DIRECTION;
 
 #endif
