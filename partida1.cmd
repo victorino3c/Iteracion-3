2d6caade0b8da35fee724ff8ec2437a432c85bd4
victorino3c <99192011+victorino3c@users.noreply.github.com>
2022-04-02 11:48:16 +0200

    Add files via upload

---
+++ partida1.cmd
@@ -0,0 +1,11 @@
+move s 
+move s 
+move e 
+move e 
+move e 
+move e 
+inspect s 
+take Grano2 
+inspect Grano2 
+drop Grano2 
+exit 
---
+++ partida2.cmd
@@ -0,0 +1,11 @@
+s 
+s 
+d 
+d 
+d 
+d 
+i s 
+c Grano2 
+i Grano2 
+v Grano2 
+exit 
