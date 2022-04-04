0bb643194f16ca71295af57da665bffec1845749
Pingu9191 <99191235+Pingu9191@users.noreply.github.com>
2022-04-04 23:52:51 +0200

    Add files via upload

---
+++ Tests/game_test.h
@@ -0,0 +1,86 @@
+/** 
+ * @brief It declares the tests for the game module
+ * 
+ * @file game_test.h
+ * @author Ignacio NUnnez
+ * @version 2.0 
+ * @date 04-04-2022
+ * @copyright GNU Public License
+ */
+
+#ifndef GAME_TEST_H
+#define GAME_TEST_H
+
+void test1_game_alloc2();
+
+void test1_game_destroy();
+
+void test2_game_destroy();
+
+void test1_game_create();
+
+void test2_game_create();
+
+void test1_game_update();
+
+void test2_game_update();
+
+void test1_game_is_over();
+
+void test2_game_is_over();
+
+void test1_game_get_space();
+
+void test2_game_get_space();
+
+void test1_game_get_object();
+
+void test2_game_get_object();
+
+void test1_game_get_player();
+
+void test2_game_get_player();
+
+void test1_game_get_enemy();
+
+void test2_game_get_enemy();
+
+void test1_game_get_player_location();
+
+void test2_game_get_player_location();
+
+void test1_game_get_enemy_location();
+
+void test2_game_get_enemy_location();
+
+void test1_game_get_object_location();
+
+void test2_game_get_object_location();
+
+void test1_game_get_last_command();
+
+void test2_game_get_last_command();
+
+void test1_game_get_enemy_id();
+
+void test2_game_get_enemy_id();
+
+void test1_game_get_player_id();
+
+void test2_game_get_player_id();
+
+void test1_game_get_object_id();
+
+void test2_game_get_object_id();
+
+void test1_game_get_connection_status();
+
+void test2_game_get_connection_status();
+
+void test1_game_get_connection();
+
+void test2_game_get_connection();
+
+void test1_game_get_description();
+
+#endif
