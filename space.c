ccc4973df58cce29cf386d6fde6b4b877ee4d411
msoto5 <99289966+msoto5@users.noreply.github.com>
2022-04-05 17:59:03 +0200

    Update space.c

--- space.c
+++ space.c
@@ -15,7 +15,7 @@
 
 #include "space.h"
 
-#define MAX_LINKS_SPACE 4
+#define MAX_LINKS_SPACE 4   /*!< Establish the maximun of links per space. This number is related to the number of the different existing direction (declared in types.h)*/
 
 /**
  * @brief Space
