95c9461d266d811becb144994667dec4d9a69e05
msoto5 <99289966+msoto5@users.noreply.github.com>
2022-04-05 18:07:07 +0200

    Update types.h

--- types.h
+++ types.h
@@ -11,29 +11,41 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#define WORD_SIZE 1000
-#define NO_ID -1
-#define MAX_ARG 20
+#define WORD_SIZE 1000    /*!< Establish the length for some strings */
+#define NO_ID -1          /*!< Establish a non-defined id and its value is -1*/
+#define MAX_ARG 20        /*!< Establish a length of command input arguments */
 
 #define FD_ID_SPACE 1  /* <! First digit of a space  */
 #define FD_ID_PLAYER 2 /* <! First digit of a player  */
 #define FD_ID_OBJ 3    /* <! First digit of an object*/
 #define FD_ID_ENEMY 4  /* <! First digit of an enemy*/
 
+/**
+ * @brief Ids
+ */
 typedef long Id;
 
+/**
+ * @brief Booleans (TRUE or FALSE)
+ */
 typedef enum
 {
   FALSE,
   TRUE
 } BOOL;
 
+/**
+ * @brief Status (OK or ERROR)
+ */
 typedef enum
 {
   ERROR = 0,
   OK = 1
 } STATUS;
 
+/**
+ * @brief Link's and space's directions (N, S, E, W or ND (not-defined))
+ */
 typedef enum
 {
   ND = -1, /* !< Short for Not Defined */
